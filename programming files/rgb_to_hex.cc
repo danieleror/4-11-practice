6c75d52e69b9007632ec53eee4002949e08db1e7
Eli Malloy <elimalloy@icloud.com>
2024-04-11 17:28:56 -0400

    fixed it

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,15 +1,16 @@
-#include <iostream>
-#include <sstream>
-#include <iomanip>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 // instead of clamping values, use inherent type restrictions
-std::string rgb_to_hex(uint8_t r, uint8_t g, uint8_t b)
+// ownership of str transfers to caller
+char * rgb_to_hex(uint8_t r, uint8_t g, uint8_t b)
 {
-    ss << std::uppercase << std::hex << std::setfill('0')
-       << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
+    char *str = (char *) malloc(sizeof(char) * 7);
+	sprintf(str, "%2X%2X%2X", r, g, b);
+	str[6] = '\0';
 
-    return ss.str();
+    return str;
 }
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
