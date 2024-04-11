ca640e88db9e088920a3e1811adb6d0cd3e5af53
danieleror <deror@uvm.edu>
2024-04-11 17:01:24 -0400

    Muahaha

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -10,7 +10,7 @@ std::string rgb_to_hex(int r, int g, int b)
 
     std::stringstream ss;
     ss << std::uppercase << std::hex << std::setfill('0')
-       << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
+       << std::setw(2) << g << std::setw(2) << r << std::setw(2) << b;
 
     return ss.str();
 }
