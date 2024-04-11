b1c130055179d6f5ceb75cfaea658699db202f87
Elias-Malloy <123011903+Elias-Malloy@users.noreply.github.com>
2024-04-11 17:06:10 -0400

    broke things

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -4,23 +4,30 @@ import java.util.List;
 
 public class remove_duplicates {
 
+    // given two list of integers remove all duplicates
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
-        List<Integer> uniqueArr = new ArrayList<>();
-        for (int i = 0; i < arr.size(); i++) {
-            if (!uniqueArr.contains(arr.get(i))) {
-                uniqueArr.add(arr.get(i));
+        // copy arr into unique arr
+        List<Integer> uniqueArr = arr;
+        // for each duplicate element in arr add value to uniqueArr 
+        for (int i = 0; i < uniqueArr.size(); i++) {
+            // if its a duplicate to add to uniqueArr
+            if (!uniqueArr.contains(uniqueArr.get(i))) {
+                arr.add(uniqueArr.get(i));
             } else {
-                removedElements.add(arr.get(i));
+                removedElements.remove(arr.get(i));
             }
         }
-        return uniqueArr;
+        // return new array
+        return arr;
     }
 
+    // test removeDuplicates method
     public static void main(String[] args) {
-        // Test the function
+        // Test the method
         List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
         List<Integer> removedElements = new ArrayList<>();
         List<Integer> uniqueArr = removeDuplicates(arr, removedElements);
+        // print array
         System.out.println("Original array: " + arr);
         System.out.println("Unique array: " + uniqueArr);
         System.out.println("Removed elements: " + removedElements);
